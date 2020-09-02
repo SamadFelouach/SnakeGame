ad1d526d7e17a9097c4f2ad93d4d994fb947e6ed
ABDESSAMAD FELOUACH <abdessamadfelouach@gmail.com>
2020-09-02 12:58:18 +0100

    set up the screen

---
+++ script.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 12:43:06 2020
+
+@author: Avicii
+"""
+import turtle
+#set up the screen
+window =turtle.Screen()
+window.title("my amazing game by @Avicci")
+window.bgcolor("green")
+window.setup(width=600 , height =500)
+window.tracer(0)
+window.mainloop()
